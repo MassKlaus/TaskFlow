9c89b46968b7ee38905eee277771187f30fc40c9
MassKlaus <massine@massinemouha.com>
2026-05-02 16:58:44 +0100

    fix: using email instead of username

--- db/user.js
+++ db/user.js
@@ -1,11 +1,11 @@
 import mongoose from "mongoose";
 
-const user = new mongoose.Schema({
-    username: { type: String, required: true, unique: true },
+const userSchema = new mongoose.Schema({
+    email: { type: String, required: true, unique: true },
     passwordHash: { type: String, required: true },
     fullName: { type: String, required: true, default: "" },
     createdAt: { type: Date, default: Date.now },
     updatedAt: { type: Date, default: Date.now },
 });
 
-export default mongoose.model("User", user);
+export default mongoose.model("User", userSchema);
